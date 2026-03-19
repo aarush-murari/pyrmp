8b2742c47dc33658e2f3c2987fddb7268a1d940d
Aarush Murari <aarush.murari@gmail.com>
2026-03-19 14:46:26 -0700

    Bump to 0.1.4, fix license to MPL-2.0, update docs version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = "pyrmp"
 copyright = "2026, Aarush Murari"
 author = "Aarush Murari"
-release = "0.1.2"
+release = "0.1.4"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
